c27f31018a74ff48c478a7e7ccf98f54a17c45fe
hoang227 <hoangg227@gmail.com>
2024-04-01 14:43:51 +0800

    add dropdown menu and constants.js file for global constants

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const transactionViewOptions = ["yearly", "monthly", "daily"];
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>index page!</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">
+      summary
+    </h1>
+    <div>
+      <USelectMenu
+        v-model="selectedView"
+        :options="transactionViewOptions"
+      />
+    </div>
+  </section>
 </template>
+
+<script setup>
+import { transactionViewOptions } from '~/constants'
+const selectedView = ref(transactionViewOptions[1])
+</script>
