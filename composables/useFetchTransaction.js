0aba7ca7ad1624ce5b12ba1baa8cafb3960aec7e
hoang227 <hoangg227@gmail.com>
2024-04-04 15:25:53 +0800

    edit useFetchTransaction to use time period provided

--- composables/useFetchTransaction.js
+++ composables/useFetchTransaction.js
@@ -1,4 +1,4 @@
-export const useFetchTransaction = () => {
+export const useFetchTransaction = (period) => {
   const supabase = useSupabaseClient()
   const transactions = ref([])
   const pending = ref(false)
@@ -23,10 +23,12 @@ export const useFetchTransaction = () => {
   const fetchTransactions = async () => {
     pending.value = true
     try {
-      const { data } = await useAsyncData("transactions", async () => {
+      const { data } = await useAsyncData(`transactions-${period.value.from.toDateString()}-${period.value.to.toDateString()}`, async () => {
         const { data, error } = await supabase
           .from("transactions")
           .select()
+          .gte('created_at', period.value.from.toISOString())
+          .lte('created_at', period.value.to.toISOString())
           .order("created_at", { ascending: false })
         if (error) return []
 
