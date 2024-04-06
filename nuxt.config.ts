b42cdbb14d01850e990178f6d6115b2f8d626cc2
hoang227 <hoangg227@gmail.com>
2024-04-06 21:50:05 +0800

    add runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,10 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   supabase: {
     redirect: false
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL ?? 'http://localhost:3000'
+    }
   }
 })
