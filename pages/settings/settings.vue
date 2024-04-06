776f25183a98fc9206533e6deaa49255dea40052
hoang227 <hoangg227@gmail.com>
2024-04-06 20:23:10 +0800

    add functionality to settings/settings

--- pages/settings/settings.vue
+++ pages/settings/settings.vue
@@ -1,7 +1,7 @@
 <template>
-  <UForm :state="state" :schema="schema" @submit.prevent="saveSettings">
+  <UForm :state="state" :schema="schema" @submit="saveSettings">
     <UFormGroup label="Transaction View" class="mb-4" help="Choose how you would like to view transactions">
-      <USelect v-model="state.transactionView" />
+      <USelect v-model="state.transactionView" :options="transactionViewOptions"/>
     </UFormGroup>
 
     <UButton type="submit" color="black" variant="solid" label="Save" :loading="pending" :disabled="pending" />
@@ -10,32 +10,35 @@
 
 <script setup>
 import { z } from 'zod'
+import { transactionViewOptions } from '~/constants'
 
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
 const { toastSuccess, toastError } = useAppToast()
 const pending = ref(false)
 const state = ref({
-  transactionView: null // Initialization goes here
+  transactionView: user.value.user_metadata?.transaction_view ?? transactionViewOptions[1]
 })
 const schema = z.object({
-  transactionView: null // ZOD constraints goes here
+  transactionView: z.enum(transactionViewOptions)
 })
 
 const saveSettings = async () => {
   pending.value = true
 
   try {
-    const { error } = await supabase.auth.updateUser(
-      // The data goes here
-    )
+    const { error } = await supabase.auth.updateUser({
+      data: {
+        transaction_view: state.value.transactionView
+      }
+    })
     if (error) throw error
     toastSuccess({
-      title: 'Settings updated',
+      title: 'settings updated',
     })
   } catch (error) {
     toastError({
-      title: 'Error updating settings',
+      title: 'error updating settings',
       description: error.message,
     })
   } finally {
